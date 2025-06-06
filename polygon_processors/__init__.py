4bb8c5b3228c23e9a9cfac735184cd15bdf85931
fportalesor <fportales.or@gmail.com>
2025-06-06 12:14:27 +0100

    update init file

--- polygon_processors/__init__.py
+++ polygon_processors/__init__.py
@@ -6,7 +6,10 @@ from .densifier import PolygonDensifier
 from .hidden_polys import HiddenPolygonProcessor
 from .split_processor import PolygonSplitter
 from .attributes_processor import AttributeCalculator
-from .overlapping_points import OverlappingPointHandler
+from .create_points import PointCreator
+from .dissolve_processor import PolygonDissolver
+from .plotter import PolygonPlotter
+from .parallel_voronoi import ParallelVoronoiProcessor
 
 __all__ = [
     'PolygonProcessor',
@@ -17,5 +20,8 @@ __all__ = [
     'HiddenPolygonProcessor',
     'PolygonSplitter',
     'AttributeCalculator',
-    'OverlappingPointHandler'
+    'PointCreator',
+    'PolygonDissolver',
+    'PolygonPlotter',
+    'ParallelVoronoiProcessor'
 ]
