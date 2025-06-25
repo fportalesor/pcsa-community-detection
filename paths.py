b05e2f71c23bb64aeee196d69cc2e416e6cf13a2
fportalesor <fportales.or@gmail.com>
2025-06-25 13:50:29 +0100

    Centralise common path variables in a shared config module for reuse

---
+++ paths.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+def get_paths():
+    base_dir = Path(__file__).parent
+    input_dir = base_dir / "data/raw"
+    output_dir = base_dir / "data/processed"
+    aztool_dir = base_dir / "data/AZTool"
+    output_dir.mkdir(exist_ok=True)
+    return input_dir, output_dir, aztool_dir
+
+if __name__ == '__main__':
+    get_paths()
